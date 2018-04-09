72bc4f6a5414eb61b2468c4784a7674461651131
Raymond Gauthier <jraygauthier@gmail.com>
2018-04-09 11:55:47 -0400

    teamviewer: 12.0.76279 -> 12.0.93330
    Also changed to source url which was broken.

--- pkgs/applications/networking/remote/teamviewer/default.nix
+++ pkgs/applications/networking/remote/teamviewer/default.nix
@@ -16,13 +16,13 @@ in
 
 stdenv.mkDerivation rec {
   name = "teamviewer-${version}";
-  version = "12.0.76279";
+  version = "12.0.93330";
 
   src = fetchurl {
     # There is a 64-bit package, but it has no differences apart from Debian dependencies.
     # Generic versioned packages (teamviewer_${version}_i386.tar.xz) are not available for some reason.
-    url = "http://download.teamviewer.com/download/teamviewer_${version}_i386.deb";
-    sha256 = "15yhx66zxbjk0x3dpfg39gb1f2ajcp9kbp4zi58bfnvby277jl00";
+    url = "https://dl.tvcdn.de/download/version_12x/teamviewer_${version}_i386.deb";
+    sha256 = "1jhghjnbw48p6hp0x20q7m2dxb4jzhasszi3lrns9qhc2apfdy85";
   };
 
   unpackPhase = ''
