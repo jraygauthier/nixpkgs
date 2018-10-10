d16f0e664312dd64c4fca3d0e913256954348d61
Thomas Bereknyei <tomberek@gmail.com>
2018-10-09 22:54:37 -0400

    guacamole: init at 0.9.14

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -630,6 +630,7 @@
   ./services/web-apps/atlassian/crowd.nix
   ./services/web-apps/atlassian/jira.nix
   ./services/web-apps/frab.nix
+  ./services/web-apps/guacamole.nix
   ./services/web-apps/mattermost.nix
   ./services/web-apps/nixbot.nix
   ./services/web-apps/nexus.nix
