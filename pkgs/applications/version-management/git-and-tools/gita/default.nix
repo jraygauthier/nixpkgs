87f79f49fe4c1cf65e3a2adc2879228b05ac2b1e
Raymond Gauthier <jraygauthier@gmail.com>
2019-11-08 11:02:59 -0500

    gita: add support for bash completions

--- pkgs/applications/version-management/git-and-tools/gita/default.nix
+++ pkgs/applications/version-management/git-and-tools/gita/default.nix
@@ -9,11 +9,21 @@ python3Packages.buildPythonApplication rec {
     sha256 = "0ccqjf288513im7cvafiw4ypbp9s3z0avyzd4jzr13m38jrsss3r";
   };
 
+  bashCompletionScript = builtins.fetchurl {
+    name = "gita-bash-completion";
+    url = https://raw.githubusercontent.com/nosarthur/gita/a37d676d90ba85364dbe80307070338360f09041/.gita-completion.bash;
+    sha256 = "0zzqsjrdg6dakyzfz3jjdhcck5nw6yx1ab75ggxbf38ypbxrf9al";
+  };
+
   propagatedBuildInputs = with python3Packages; [
     pyyaml
   ];
 
   doCheck = false;  # Releases don't include tests
+
+  postInstall = ''
+    install -D "${bashCompletionScript}" "$out/share/bash-completion/completions/gita"
+  '';
 
   meta = with lib; {
     description = "A command-line tool to manage multiple git repos";
