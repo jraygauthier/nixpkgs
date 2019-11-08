19103a4f38042fd42c3a070267a50249345bb084
Raymond Gauthier <jraygauthier@gmail.com>
2019-11-08 10:42:47 -0500

    gita: add support for bash completions (duplicate)

--- pkgs/development/python-modules/gita/default.nix
+++ pkgs/development/python-modules/gita/default.nix
@@ -11,9 +11,19 @@ buildPythonPackage rec {
     sha256 = "0j0b788dw0c0wyn8wwgx2iiw024vm10fxqyrxhnshs3gnsba51rp";
   };
 
+  bashCompletionScript = builtins.fetchurl {
+    name = "gita-bash-completion";
+    url = https://raw.githubusercontent.com/nosarthur/gita/a37d676d90ba85364dbe80307070338360f09041/.gita-completion.bash;
+    sha256 = "0zzqsjrdg6dakyzfz3jjdhcck5nw6yx1ab75ggxbf38ypbxrf9al";
+  };
+
   propagatedBuildInputs = [ pyyaml_5 ];
 
   doCheck = true;
+
+  postInstall = ''
+    install -D "${bashCompletionScript}" "$out/share/bash-completion/completions/gita"
+  '';
 
   meta = with stdenv.lib; {
     description = "Manage multiple git repos";
