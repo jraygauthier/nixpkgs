cb996222d2fb774108bc2d095d20708e834841c9
Raymond Gauthier <jraygauthier@gmail.com>
2022-06-09 16:43:31 -0400

    tiscamera: fix binaries and udev rules
     -  Patch udev rules
     -  Add our own out as GST plugin path and GI plugin path
     -  Fix 'tcam-capture' qt gui

--- pkgs/os-specific/linux/tiscamera/default.nix
+++ pkgs/os-specific/linux/tiscamera/default.nix
@@ -3,6 +3,7 @@
 , fetchFromGitHub
 , cmake
 , pkg-config
+, runtimeShell
 , pcre
 , tinyxml
 , libusb1
@@ -18,7 +19,11 @@
 , libuuid
 , wrapGAppsHook
 , catch2
+, libsForQt5
+, withGui ? true
 }:
+
+with lib;
 
 stdenv.mkDerivation rec {
   pname = "tiscamera";
@@ -33,6 +38,11 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     cp ${catch2}/include/catch2/catch.hpp external/catch/catch.hpp
+
+    substituteInPlace ./data/udev/80-theimagingsource-cameras.rules.in \
+      --replace "/bin/sh" "${runtimeShell}/bin/sh" \
+      --replace "typically /usr/bin/" "" \
+      --replace "typically /usr/share/theimagingsource/tiscamera/uvc-extension/" ""
   '';
 
   nativeBuildInputs = [
@@ -40,6 +50,8 @@ stdenv.mkDerivation rec {
     pkg-config
     python3Packages.wrapPython
     wrapGAppsHook
+  ] ++ optionals withGui [
+    libsForQt5.wrapQtAppsHook
   ];
 
   buildInputs = [
@@ -51,18 +63,26 @@ stdenv.mkDerivation rec {
     gobject-introspection
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
     libwebcam
     libunwind
     elfutils
     orc
     libuuid
     python3Packages.python
+  ] ++ optionals withGui [
     python3Packages.pyqt5
+    libsForQt5.qtbase
   ];
 
   pythonPath = with python3Packages; [ pyqt5 pygobject3 ];
 
   propagatedBuildInputs = pythonPath;
+
+  patches = [
+    ./0001-Remove-wrong-volatile-usage-with-g_once_init_enter.patch
+  ];
 
   cmakeFlags = [
     "-DBUILD_ARAVIS=OFF" # For GigE support. Won't need it as our camera is usb.
@@ -71,6 +91,7 @@ stdenv.mkDerivation rec {
     "-DBUILD_V4L2=ON"
     "-DBUILD_LIBUSB=ON"
     "-DBUILD_TESTS=ON"
+    "-DTCAM_BUILD_WITH_GUI=${if withGui then "ON" else "OFF"}"
     "-DTCAM_INSTALL_UDEV=${placeholder "out"}/lib/udev/rules.d"
     "-DTCAM_INSTALL_UVCDYNCTRL=${placeholder "out"}/share/uvcdynctrl/data/199e"
     "-DTCAM_INSTALL_GST_1_0=${placeholder "out"}/lib/gstreamer-1.0"
@@ -90,11 +111,24 @@ stdenv.mkDerivation rec {
   # gstreamer tests requires, besides gst-plugins-bad, plugins installed by this expression.
   checkPhase = "ctest --force-new-ctest-process -E gstreamer";
 
+  # wrapGAppsHook: make sure we add ourselves to the introspection
+  # and gstreamer paths.
+  GI_TYPELIB_PATH = "${placeholder "out"}/lib/girepository-1.0";
+  GST_PLUGIN_SYSTEM_PATH_1_0 = "${placeholder "out"}/lib/gstreamer-1.0";
+
+  QT_PLUGIN_PATH = optionalString withGui "${libsForQt5.qtbase.bin}/${libsForQt5.qtbase.qtPluginPrefix}";
+
+  dontWrapQtApps = true;
+
+  preFixup = ''
+    gappsWrapperArgs+=("''${qtWrapperArgs[@]}")
+  '';
+
   postFixup = ''
     wrapPythonPrograms "$out $pythonPath"
   '';
 
-  meta = with lib; {
+  meta = {
     description = "The Linux sources and UVC firmwares for The Imaging Source cameras";
     homepage = "https://github.com/TheImagingSource/tiscamera";
     license = with licenses; [ asl20 ];
